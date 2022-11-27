42dd025a50d57a1920a3189f67d5f623d0ec35b7
Smith Ringtho <sringtho@gmail.com>
2022-11-27 20:07:43 +0300

    Added payment modal and final 'thank you' display pages

--- index.js
+++ index.js
@@ -4,16 +4,58 @@ document.addEventListener('click', function(e){
     if (e.target.dataset.addorder){
         handleAddOrderClick(e.target.dataset.addorder)
     }
+    else if (e.target.id === 'order-btn'){
+        handleCompleteOrder()    
+    }
+    else if (e.target.id === 'pay-btn'){
+        handlePayment()
+        
+    }
 })
 
 function handleAddOrderClick(menuId){
-    console.log("clicked")
+    let orderHtml = ``
+    let totalPrice = 0
+    const menuObj = menuArray.filter(function(item){
+        return item.id == menuId
+    })[0]
+
+    orderHtml += `
+    <div class="order-container" id="order-container">
+        <h4 class="food-name">${menuObj.name}</h4>
+        <small class="remove-btn verdana mg-rg-auto" id="remove-btn">remove</small>
+        <p class="food-price">$${menuObj.price}</p>
+    </div>
+    `
+    
+    totalPrice += menuObj.price
+
+    const priceHtml = `
+    <h4 class="total-price mg-rg-auto">Total Price:</h4>
+    <p class="food-price">$${totalPrice}</p>
+    `
+
+    document.getElementById("food-order-container").classList.remove("hidden")
+    document.getElementById("orders").innerHTML = orderHtml
+    document.getElementById("total-price").innerHTML = priceHtml
+    
+}
+
+function handleCompleteOrder(){
+    document.getElementById('modal').classList.remove('hidden')
+}
+
+function handlePayment(){
+    document.getElementById('food-order-container').classList.add('hidden')
+    document.getElementById('modal').classList.add('hidden')
+    document.getElementById('thank-you').classList.remove('hidden')
 }
 
 function getMenu(){
-    let displayHtml = ''
+    let menuHtml = ``
+
     menuArray.forEach(function(item){
-        let menuHtml = `
+        menuHtml += `
         <section class="food-wrapper">
             <div class="food-container">
                 <img src="${item.emoji}" alt="${item.name}">
@@ -28,8 +70,34 @@ function getMenu(){
             </div>
         </section>
         `
-        displayHtml += menuHtml
     })
+
+    const orderHtml = `
+    <section class="food-order-container hidden" id="food-order-container">
+        <h3 class="order-header">Your Order</h3>
+        <div class="orders" id="orders">
+        </div>
+        <div id="total-price" class="price-container">
+        </div>
+        <button id="order-btn" class="verdana">Complete order</button>
+    </section>
+        
+    `
+
+    const thankYouHtml = `
+    <section class="hidden" id="thank-you">
+        <div class="thank-you">
+            <h4>Thanks James! Your order is on its way!</h4>
+        </div>
+    </section>
+    `
+    const displayHtml = `
+        ${ menuHtml }
+        ${ orderHtml }
+        ${ thankYouHtml }
+
+    `
+
     return displayHtml
 }
 
