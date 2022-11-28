c9338fbbd5ce89629c56da5e0a9df0f2ccbccc73
Smith Ringtho <sringtho@gmail.com>
2022-11-28 09:21:27 +0300

    Added delete functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { menuArray } from "./data";
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 let orderArr = []
 
 document.addEventListener('click', function(e){
@@ -11,39 +12,26 @@ document.addEventListener('click', function(e){
     else if (e.target.id === 'pay-btn'){
         handlePayment()
         
-    } else if (e.target.id === 'remove-btn'){
-        handleRemoveItem()
+    } else if (e.target.dataset.removeorder){
+        handleRemoveItem(e.target.dataset.removeorder)
     }
 })
 
 function handleAddOrderClick(menuId){
-    let totalPrice = 0
+    
     const menuObj = menuArray.filter(function(item){
         return item.id == menuId
     })[0]
 
-    orderArr.push({name: menuObj.name, price: menuObj.price})
-
-    let orderHtml = ``
-    for (let order of orderArr){
-        orderHtml += `
-        <div class="order-container" id="order-container">
-            <h4 class="food-name">${order.name}</h4>
-            <small class="remove-btn verdana mg-rg-auto" id="remove-btn">remove</small>
-            <p class="food-price">$${order.price}</p>
-        </div>
-        `
-        totalPrice += order.price
-    }
-
-    const priceHtml = `
-    <h4 class="total-price mg-rg-auto">Total Price:</h4>
-    <p class="food-price">$${totalPrice}</p>
-    `
+    orderArr.push(
+        {
+            name: menuObj.name, 
+            price: menuObj.price, 
+            id: menuId,
+            uuid: uuidv4()
+        })
+    renderMenu()
     document.getElementById("food-order-container").classList.remove("hidden")
-    document.getElementById("orders").innerHTML = orderHtml
-    document.getElementById("total-price").innerHTML = priceHtml
-    
 }
 
 function handleCompleteOrder(){
@@ -51,13 +39,18 @@ function handleCompleteOrder(){
 }
 
 function handlePayment(){
-    document.getElementById('food-order-container').classList.add('hidden')
+    document.getElementById("food-order-container").classList.add('hidden')
     document.getElementById('modal').classList.add('hidden')
     document.getElementById('thank-you').classList.remove('hidden')
 }
 
-function handleRemoveItem(){
-    console.log("We shall soon remove an item...")
+function handleRemoveItem(id){
+    const orders = orderArr.filter(function(order){
+        return order.id != id
+    })
+    orderArr = orders
+    renderMenu()
+    document.getElementById("food-order-container").classList.remove('hidden')
 }
 
 function getMenu(){
@@ -81,12 +74,30 @@ function getMenu(){
         `
     })
 
+
+    let totalPrice = 0
+    let orderIter = ``
+
+    orderArr.forEach(function(order){
+        orderIter += `
+        <div class="order-container" id="order-container">
+            <h4 class="food-name">${order.name}</h4>
+            <small class="remove-btn verdana mg-rg-auto" data-removeorder="${order.id}" id="remove-btn">remove</small>
+            <p class="food-price">$${order.price}</p>
+        </div>
+        `
+        totalPrice += order.price
+    })
+
     const orderHtml = `
     <section class="food-order-container hidden" id="food-order-container">
         <h3 class="order-header">Your Order</h3>
         <div class="orders" id="orders">
+            ${orderIter}
         </div>
         <div id="total-price" class="price-container">
+            <h4 class="total-price mg-rg-auto">Total Price:</h4>
+            <p class="food-price">$${totalPrice}</p>
         </div>
         <button id="order-btn" class="verdana">Complete order</button>
     </section>
